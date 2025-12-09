d68f7cc8a7068312431d62d47f211dff6404249e
Dilip <diliptalks@gmail.com>
2025-12-09 12:20:35 +0100

    fix for CORS error, added frontend to the CORS orgins list

--- server/core/config.py
+++ server/core/config.py
@@ -12,8 +12,8 @@ class Settings(BaseSettings):
     ENAM_PRICES_COLLECTION: str = "enam_prices"
 
     CORS_ORIGINS: List[str] = [
-        "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://localhost:5173",
+        "http://127.0.0.1:5173"
     ]
 
     class Config:
